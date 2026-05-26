11fbe7a4aae7dc64f9a49dca5802d684bfb7a8fa
Pradeep Vontisubramanyam <pradeepvonti@aksdataai.com>
2026-05-25 21:38:24 -0400

    Fix: bypass and unregister service workers in local flutter-preview to prevent stale cache

--- public/flutter-preview/flutter_bootstrap.js
+++ public/flutter-preview/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1301593128" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1795950738" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
